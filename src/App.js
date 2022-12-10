2adf65ffc99d6eb3ae5bd35d034f0657ced6a55d
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2022-12-10 19:24:12 +0530

    Add

--- src/App.js
+++ src/App.js
@@ -1,32 +1,64 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 function App() {
 
-  const [images, setImages] = useState([]);
-  const [currImageIdx, setCurrImageIdx] = useState(0);
+  const [inputs, setInputs] = useState([""]);
 
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/photos')
-      .then(res => res.json())
-      .then(data => setImages(data))
-  }, []);
-
-  const handlePrevious = () => {
-    if (currImageIdx > 0) {
-      setCurrImageIdx(currImageIdx - 1);
+  const handleAdd = () => {
+    if (inputs.length >= 10) {
+      return;
     }
+    setInputs([...inputs, ""]);
   };
-  const handleNext = () => {
-    if (currImageIdx < images.length - 1) {
-      setCurrImageIdx(currImageIdx + 1);
+
+  const handleRemove = (idx) => {
+    if (inputs.length <= 1) {
+      return;
     }
+    setInputs(inputs.filter((_, i) => i !== idx));
+  };
+
+  const handleChange = (event, idx) => {
+    const updatedInputs = [...inputs];
+    updatedInputs[idx] = event.target.value;
+    setInputs(updatedInputs);
   };
 
   return (
     <div>
-      <button onClick={handlePrevious}>Previous</button>
-      <img src={images[currImageIdx].url} alt="carousel" />
-      <button onClick={handleNext}>Next</button>
+      {
+        inputs.map((input, idx) => (
+          <div key={idx} style={{
+            margin: "10px"
+          }}>
+            <input type="text" value={input} onChange={(e) => handleChange(e, idx)}
+              style={{
+                border: "3px solid black",
+                borderRadius: "4px"
+              }}
+            />
+            <button onClick={handleAdd} style={{
+              padding: "10px",
+              marginLeft: "10px",
+              border: "none",
+              borderRadius: "50%",
+              cursor: "pointer",
+              backgroundColor: "#007BFB",
+              color: "white"
+            }}>+</button>
+
+            <button onClick={() => handleRemove(idx)} style={{
+              padding: "10px",
+              marginLeft: "10px",
+              border: "none",
+              borderRadius: "50%",
+              cursor: "pointer",
+              backgroundColor: "#ED313F",
+              color: "white"
+            }}>x</button>
+          </div>)
+        )
+      }
     </div>
   );
 }
